137c3265c6fc13d1ae4dfd149af4f535decb510c
mudassirneyaz <mudassirneyaz000@gmail.com>
2020-06-09 01:53:37 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,88 @@
+  <!-- Site footer -->
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-6 col-md-3">
+            <h6>College</h6>
+            <ul class="footer-links">
+              <li><a href="http://scanfcode.com/category/c-language/">Campus</a></li>
+              <li><a href="http://scanfcode.com/category/front-end-development/">News & Events</a></li>
+              <li><a href="http://scanfcode.com/category/back-end-development/">Gallery</a></li>
+              <li><a href="http://scanfcode.com/category/java-programming-language/">Contact</a></li>
+              <li><a href="http://scanfcode.com/category/android/">Testimonials</a></li>
+              <li><a href="http://scanfcode.com/category/templates/">Notice Board, E-library</a></li>
+              <li><a href="http://scanfcode.com/category/android/">Infrastructure</a></li>
+            </ul>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Quick Links</h6>
+            <ul class="footer-links">
+              <li><a href="http://scanfcode.com/category/c-language/">About</a></li>
+              <li><a href="http://scanfcode.com/category/front-end-development/">Admission</a></li>
+              <li><a href="http://scanfcode.com/category/back-end-development/">Student</a></li>
+              <li><a href="http://scanfcode.com/category/java-programming-language/">Exams</a></li>
+              <li><a href="http://scanfcode.com/category/android/">Career</a></li>
+            </ul>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Others</h6>
+            <ul class="footer-links">
+              <li><a href="http://scanfcode.com/category/c-language/">Anti Ragging</a></li>
+              <li><a href="http://scanfcode.com/category/front-end-development/">RTI </a></li>
+              <li><a href="http://scanfcode.com/category/back-end-development/">Grievances</a></li>
+              <li><a href="http://scanfcode.com/category/java-programming-language/">Prospectus</a></li>            </ul>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Address</h6>
+            <ul class="contact-list">
+                                            <li>H. No. 296, Purani Basti Road<br> Panchi Mohalla, Jugsalai - 831006,<br>Jamshedpur, Jharkhand, India </li>
+                                           <li> <a href="tel:7361 810 811">+91-7361 810 811</a> / <a href="tel:7361 810 818">7361 810 818</a></li>
+                                <li><a href="mailto:info@xyz.com">info@xyz.com</a></li>
+                                <li><a href="mailto:info@xyz.com">contact@xyz.com</a></li>
+                                        </ul>
+          </div>
+        </div>
+        <hr>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2020 All Rights Reserved by 
+         <a href="#">NMIS</a>.
+            </p>
+          </div>
+
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <ul class="social-icons">
+              <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+              <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
+            </ul>
+          </div>
+        </div>
+      </div>
+</footer>
+
+
+
+      <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Contact form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/agency.min.js"></script>  
+     </body>
+    
+
+     </html>
